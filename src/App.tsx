4ac041996a184206dc0e5c5f50ae779471a41371
Bobokhuja <bobokhuja@yandex.ru>
2022-08-06 09:15:38 +0500

    create page home

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import classes from './App.module.scss'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout/Layout'
+import Home from './views/Home/Home'
 
 function App() {
   return (
     <div className={classes.App}>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </div>
