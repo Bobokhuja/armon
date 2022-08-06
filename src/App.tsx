f5b341c4a472ca26791fb35b4238995acae3ec92
Bobokhuja <bobokhuja@yandex.ru>
2022-08-06 09:13:38 +0500

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import classes from './App.module.scss'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
     <div className={classes.App}>
-
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index />
+        </Route>
+      </Routes>
     </div>
   )
 }
