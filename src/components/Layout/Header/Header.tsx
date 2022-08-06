e6e25697b8b7f0720e0e282bd123e6ab419fd0a8
Bobokhuja <bobokhuja@yandex.ru>
2022-08-06 09:35:52 +0500

    create mixin min-max

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,10 +1,16 @@
 import classes from './Header.module.scss'
 import Logo from '../../Logo/Logo'
 
-function Header() {
+function Header({mainPage = false}: {mainPage?: boolean}) {
+  const cls = [
+    classes.Header,
+    mainPage ? classes.MainHeader : ''
+  ]
   return (
-    <header className={classes.Header}>
-      <Logo />
+    <header className={cls.join(' ')}>
+      <div className={classes.Wrapper}>
+        <Logo />
+      </div>
     </header>
   )
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,14 @@
 import classes from './Layout.module.scss'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import Header from './Header/Header'
+import { useState } from 'react'
 
 function Layout() {
+  const location = useLocation()
+
   return (
     <div className={classes.Layout}>
+      <Header mainPage={location.pathname === '/'} />
       <Outlet />
     </div>
   )
